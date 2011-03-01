faa5f7d9f2e5faecd22f04605c0e4fb82238a136
Topher Brown <topher200@gmail.com>
2011-03-01 01:27:40 -0500

    working on first two floors

--- src/cursor_10/core.clj
+++ src/cursor_10/core.clj
@@ -1,1 +1,50 @@
 (ns cursor-10.core)
+
+(def crystals
+     ;; Vector of vectors of [X Y] coordinates (whooo) with crystal locations.
+     ;; Each vector cooresponds to a different floor. Floor counting starts at 1
+     [[]
+      [[723.0 577.0] [611.0 510.0] [754.0 415.0] [855.0 483.0]]
+      [[511.0 486.0] [727.0 612.0] [725.0 486.0] [723.0 363.0]]
+      ])
+
+(def ladders
+     ;; Vector of [X Y] coordinates for the ladder on each level.
+     ;; Floor counting starts at 1
+     [[]
+      [619.0 420.0]
+      [956.0 488.0]
+      ])
+
+(defn click-point
+  ([[x y]]
+     (dorun [(println x y)
+             (.mouseMove (java.awt.Robot.) x y)
+             (Thread/sleep 200)
+             (.mousePress (java.awt.Robot.)
+                          (.. java.awt.event.InputEvent BUTTON1_MASK))
+             (Thread/sleep 200)
+             (.mouseRelease (java.awt.Robot.)
+                          (.. java.awt.event.InputEvent BUTTON1_MASK))
+             (Thread/sleep 200)
+             ])))
+
+(defn click-points [seq]
+  (map click-point seq))
+
+(defn clear-floor [floor]
+  (click-points (nth crystals floor)))
+
+(defn move-up [floor]
+  (click-point (nth ladders floor)))
+
+(defn clear-floor-and-move-up [floor]
+  (dorun [(clear-floor floor) (move-up floor)]))
+
+(defn floor-1 []
+  (click-points 
+   (conj (nth crystals 0) (nth ladders 0))))
+
+(defn run-game []
+  ;; (map #(dorun [(clear-floor %) (move-up %)]) [1 2]))
+  (dorun [(clear-floor 1) (move-up 1) (clear-floor 2)]))
