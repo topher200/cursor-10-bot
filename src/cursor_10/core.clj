1d805bfb1e456e523edf442497a3a098e75983d7
Topher Brown <topher200@gmail.com>
2011-03-04 20:13:03 -0500

    modifying (move-up ) to take out the special case on floor 4

--- src/cursor_10/core.clj
+++ src/cursor_10/core.clj
@@ -65,10 +65,7 @@
   (click-points (nth crystals floor)))
 
 (defn move-up [floor]
-  (if (not= floor 4) ;; ladders on 4 must be clicked x2
-    (click-points (nth ladders floor))
-    (dorun [(click-points (nth ladders floor))
-            (click-points (nth ladders floor))])))
+  (click-points (nth ladders floor)))
 
 (defn hold-floor-block [floor]
   ;; assumes there is one hold-block per floor
